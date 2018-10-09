119b111e8b9eaf723e1c2ea52975d646c28aa26a
Brian Abdo <fleenyworks@gmail.com>
2018-10-09 18:51:28 -0400

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+app.use(express.static(__dirname + '/dist'));
 
-app.use(exprss.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 3000);
 
-app.listen(process.env.PORT || 8080);
+app.get ('/*', (req,res)=>{
 
-app.get ('/*', function(req,res){
-
-  res.sendFile(path.join(__dirname +'/dist/index.html'));
+  var p = path.join(__dirname +'/dist/index.html');
+  res.sendFile(p);
 });
 
 console.log('Console Listen');
