821c35bf99272d073b1f955fd0514478db3ee7dc
Brian Abdo <fleenyworks@gmail.com>
2018-10-09 19:04:17 -0400

    heroku

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/ACAE-FrontEnd'));
 
 app.listen(process.env.PORT || 3000);
 
 app.get ('/*', (req,res)=>{
 
-  var p = path.join(__dirname +'/dist/index.html');
+  var p = path.join(__dirname +'/dist/ACAE-FrontEnd/index.html');
   res.sendFile(p);
 });
 
