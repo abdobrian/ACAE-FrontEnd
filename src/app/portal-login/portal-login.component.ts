454a823efc52b8a3f0fe53930d8ebdcbfc9e67ad
Brian Abdo <fleenyworks@gmail.com>
2018-10-09 21:37:06 -0400

    Heroku Image First

--- src/app/portal-login/portal-login.component.ts
+++ src/app/portal-login/portal-login.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortalLoginComponent implements OnInit {
 
+  loginBackground = 'assets/public/images/ACA-Virtual-Platform-Login.jpg';
   constructor() { }
 
   ngOnInit() {
