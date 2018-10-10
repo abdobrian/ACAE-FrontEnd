657a97dd27a3e40bc69ef10cb3f732c52e0e5e41
Brian Abdo <fleenyworks@gmail.com>
2018-10-09 21:10:41 -0400

    Addition of images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortalLoginComponent } from './portal-login/portal-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortalLoginComponent
   ],
   imports: [
     BrowserModule
