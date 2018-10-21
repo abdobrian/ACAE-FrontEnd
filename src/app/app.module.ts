c2a92cdbb0ec4b142e0e6bbc9725d892cc41d342
Brian Abdo <fleenyworks@gmail.com>
2018-10-21 14:15:27 -0400

    Transfer 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PortalLoginComponent } from './portal-login/portal-login.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(faCoffee);
 @NgModule({
   declarations: [
     AppComponent,
     PortalLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
